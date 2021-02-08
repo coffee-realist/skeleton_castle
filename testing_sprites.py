707e79c5fb1158ecea00b616a9af87dd5375fe3d
pivendanila <piven.danila@mail.ru>
2021-02-08 10:39:38 +0700

    1 версия тестировщика

---
+++ testing_sprites.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import pygame
+
+INTRO_WINDOW_SIZE = 1280, 720
+ABOUT_WINDOW_SIZE = 1023, 649
+GAME_WINDOW_SIZE = 1280, 720
+WINDOW_STATE = 0
+FPS = 50
+image = pygame.Surface([60, 65])
+all_sprites = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+            image = image.convert_alpha()
+    return image
+
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
+if __name__ == '__main__':
+    pygame.init()
+
+    size = width, height = 500, 500
+    screen = pygame.display.set_mode(size)
+    running = True
+    hero = AnimatedSprite(load_image("heroes/hero_run.png"), 8, 1, 6, 65)
+    while running:
+        pass
+
+
